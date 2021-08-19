20c81efac167ac236bdee066dd6eb8ba89b43384
vlakondra <kondrashkin.wladimir@yandex.ru>
2021-08-19 13:27:48 +0000

    did shadow

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!!!</h1>
-	<h2>Это проверка</h2>
+	<h1>HI {name}!!!</h1>
+	<h2>это проверка</h2>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -14,11 +14,12 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2)
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #423b9e;
+		text-transform: lowercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
