2112afe3a8bd37b85bffe9998dd5f1f8044636b7
vlakondra <kondrashkin.wladimir@yandex.ru>
2021-09-28 20:54:02 +0000

    strange?

--- src/App1.svelte
+++ src/App1.svelte
@@ -2,13 +2,13 @@
 	import sum from "lodash/sum";
 	import concat from "lodash/concat";
 
-    //npm i --save lodash
-//https://www.w3schools.com/html/default.asp
-//https://only-to-top.ru/blog/coding/2020-12-30-js-array-cheatsheet.html
+	//npm i --save lodash
+	//https://www.w3schools.com/html/default.asp
+	//https://only-to-top.ru/blog/coding/2020-12-30-js-array-cheatsheet.html
 
 	import Test from "./Test.svelte";
-	import Comp1 from "./Comp1.svelte"
-	import Comp2 from "./Comp2.svelte"
+	import Comp1 from "./Comp1.svelte";
+	import Comp2 from "./Comp2.svelte";
 
 	export let arr;
 	let x = sum([1, 2, 3, 4]);
@@ -17,38 +17,41 @@
 
 	function handleMessage(event) {
 		switch (event.detail.text) {
-			case "lodash": 
+			case "lodash":
 				console.log("LODASH");
-				arr = concat(arr, {item: 'item: 999', index: 'LD'})
-				{break}
-			case "js": 
+				arr = concat(arr, { item: "item: 999", index: "LD" });
+				{
+					break;
+				}
+			case "js":
 				console.log("JS");
 				//1-й вар-т arr.push({item: 'item: 777', index: 'JS'});
-	            // arr = arr;
+				// arr = arr;
 				//2-й вар-т
-				arr = arr.concat({item: 'item: 555', index: 'JS'});
-                //3-й вар-т arr = [...arr, {item: 'item: 444', index: 'JS'}] 
-				{break}
+				arr = arr.concat({ item: "item: 555", index: "JS" });
+				//3-й вар-т arr = [...arr, {item: 'item: 444', index: 'JS'}]
+				{
+					break;
+				}
 			default:
 				console.log("??");
 		}
 	}
 
-	function tgl(){
-		toggle=!toggle
+	function tgl() {
+		toggle = !toggle;
 	}
 </script>
 
 <main>
-{#if  (toggle)}
-   <Comp1/>
-{:else}
-  <Comp2/>
-{/if}
+	{#if toggle}
+		<Comp1 />
+	{:else}
+		<Comp2 />
+	{/if}
 
-<button on:click={()=>toggle=!toggle}>Toggle</button>
-
-
+	<button on:click={() => (toggle = !toggle)}>Toggle</button>
+	
 	<table border="1px">
 		{#each arr as a}
 			<tr>
